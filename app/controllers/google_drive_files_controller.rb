9211137261a3b88f0abbe8557996099fc5e36dcc
Scanny Mascarenhas <scanny.mascarenhas@presentsoft.co.in>
2014-02-08 20:47:36 +0530

    Adds module to access google drive if user registers with devise sign up

--- app/controllers/google_drive_files_controller.rb
+++ app/controllers/google_drive_files_controller.rb
@@ -3,23 +3,27 @@ class GoogleDriveFilesController < ApplicationController
   before_action :authenticate_user!
   def index
     @current_user = current_user
-    if @current_user.access_token_expires_at < Time.now
-      uri = URI.parse('https://accounts.google.com/o/oauth2/token')
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = true
-      params = {
-          'client_id' => ENV['GOOGLE_CLIENT_ID'],
-          'grant_type' => 'refresh_token',
-          'client_secret' => ENV['GOOGLE_SECRET'],
-          'refresh_token' => @current_user.refresh_token
-      }
-      response = http.post(uri.path, URI.encode_www_form(params))
-      body = JSON.parse(response.body)
-      @current_user.update_attributes(:access_token => body['access_token'], :access_token_expires_at => Time.now + body['expires_in'])
+    if @current_user.access_token.nil?
+      @files = []
+    else
+      if @current_user.access_token_expires_at < Time.now
+        uri = URI.parse('https://accounts.google.com/o/oauth2/token')
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true
+        params = {
+            'client_id' => ENV['GOOGLE_CLIENT_ID'],
+            'grant_type' => 'refresh_token',
+            'client_secret' => ENV['GOOGLE_SECRET'],
+            'refresh_token' => @current_user.refresh_token
+        }
+        response = http.post(uri.path, URI.encode_www_form(params))
+        body = JSON.parse(response.body)
+        @current_user.update_attributes(:access_token => body['access_token'], :access_token_expires_at => Time.now + body['expires_in'])
+      end
+      client = OAuth2::Client.new(ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_SECRET'])
+      token = OAuth2::AccessToken.new(client, @current_user.access_token)
+      response = token.get('https://www.googleapis.com/drive/v2/files')
+      @files = JSON.parse(response.body)['items']
     end
-    client = OAuth2::Client.new(ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_SECRET'])
-    token = OAuth2::AccessToken.new(client, @current_user.access_token)
-    response = token.get('https://www.googleapis.com/drive/v2/files')
-    @files = JSON.parse(response.body)['items']
   end
 end
--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -2,18 +2,23 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def google_oauth2
     auth = request.env["omniauth.auth"]
-    @user = User.find_for_google_oauth2(auth)
-    #client = OAuth2::Client.new(ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_SECRET'])
-    #puts request.env['omniauth.auth']
-    if @user.persisted?
-      current_time = Time.now
-      credentials = auth.credentials
-      @user.update_attributes(:access_token => credentials.token, :refresh_token => credentials.refresh_token, :access_token_expires_at => current_time + (credentials.expires_at - current_time.to_i) )
-      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, :kind => "Google") if is_navigational_format?
+    if user_signed_in? && current_user.email != auth.info.email
+      flash[:error] = 'Google email id does not match with registered email id.'
+      redirect_to google_drive_files_path
     else
-      #session["devise.facebook_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_path
+      @user = User.find_for_google_oauth2(auth)
+      #client = OAuth2::Client.new(ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_SECRET'])
+      #puts request.env['omniauth.auth']
+      if @user.persisted?
+        current_time = Time.now
+        credentials = auth.credentials
+        @user.update_attributes(:access_token => credentials.token, :refresh_token => credentials.refresh_token, :access_token_expires_at => current_time + (credentials.expires_at - current_time.to_i) )
+        sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+        #set_flash_message(:notice, :success, :kind => "Google") if is_navigational_format?
+      else
+        #session["devise.facebook_data"] = request.env["omniauth.auth"]
+        redirect_to new_user_registration_path
+      end
     end
     #token = OAuth2::AccessToken.new(client, request.env['omniauth.auth']['credentials']['token'])
     #response = token.get('https://www.googleapis.com/drive/v2/files')
